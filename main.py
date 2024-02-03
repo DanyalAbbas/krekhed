d514036a2e606e597df5f2031d41f32c63081527
Danyal Abbas <abasdaniel61@gmail.com>
2024-02-03 12:42:42 +0500

    added collision, music and score (even tho it doesnt really work :3)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 import math
 import sys
 import webbrowser
+import random
+from pygame import mixer
+
 
 
 pygame.init()
@@ -20,6 +23,7 @@ class Krekhead():
         self.credit_img = pygame.image.load("credits.png")
         self.credit_rect = pygame.Rect(-3,-40, 120, 90)
         self.img_small = pygame.transform.scale(self.img, (50,50))
+        self.obstacle_cactus = pygame.image.load("cactus.png")
         self.scroll = 0
         
 
@@ -52,7 +56,7 @@ class Krekhead():
             action()
 
     def scroll_thingey(self):
-        background  = pygame.image.load("bg.png").convert()
+        background  = pygame.image.load("bg.png").convert_alpha()
         bg_width = background.get_width()
         tiles = math.ceil((self.window_width / bg_width)) + 1
 
@@ -66,7 +70,7 @@ class Krekhead():
         f = pygame.font.Font(font, size)
         write = f.render(text,True,color, bg_color)
         return write
-
+    
 
 class Screens():
     def __init__(self):
@@ -82,13 +86,17 @@ class Krek():
         self.y = y+main_y
         self.width = 50
         self.height = 50
+        self.img_small = pygame.transform.scale(pygame.image.load("ghost.png"), (50,50))
         self.color = (255,255,0)
+        self.score = 0
         self.jump_height = 20
         self.jump_velocity = self.jump_height
         self.move_left = False
         self.move_right = False
         self.isJump = False
         self.steps = 6
+        self.enemy_x = random.randint(900,1400)
+        self.enemy_y = 560 
 
 
     def create_character(self):
@@ -98,8 +106,9 @@ class Krek():
 
     def draw(self,win):
         rect = self.create_character()
-
         pygame.draw.rect(win,self.color,rect)
+        win.blit(self.img_small, rect.center)
+
 
     def move(self,x,y):
         self.x += x
@@ -112,6 +121,18 @@ class Krek():
             self.isJump = False
             self.jump_velocity = self.jump_height
 
+    def enemy(self,win):
+        enemy_rect = pygame.Rect(self.enemy_x - 6,self.enemy_y, 35,50)
+        pygame.draw.rect(win, (0,0,200),enemy_rect)
+    
+    def isCollision(self):
+        distance = math.sqrt((math.pow(self.enemy_x-self.x,2)) + (math.pow(self.enemy_y-self.y,2)))
+        if distance < 35:
+            return True
+        else:
+            return False
+
+
 # DIFFERENT SCREENS
 
 def start():
@@ -120,26 +141,31 @@ def start():
     return gameplay
 
 
+mixer.music.load('bg_music.mp3')
+mixer.music.play(-1)
+
+
 krek = Krekhead()
 player = Krek(krek.main_x,krek.main_y,250,550)
 win = krek.initialize()
 clock  = pygame.time.Clock()
 FPS = 60
 run = True
-
-
-
-
 gameplay  = False
+
 
 
 
 
 while run:
     if gameplay:
+        player.enemy_x -= 7
         clock.tick(FPS)
         krek.scroll_thingey()
         # win.blit(img_small, (300, 100))
+        win.blit(krek.text_render('Slowdex.ttf', 25, f"SCORE : {player.score}", (0,255,0), (0,0,0)), (600,25))
+
+
 
         win.blit(krek.text_render('freesansbold.ttf', 25, "nigga", (0,255,0), (0,0,0)), (krek.window_width-450, krek.window_height - 400))
         for event in pygame.event.get():
@@ -147,20 +173,28 @@ while run:
                     run = False
 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                         player.move_left = True
-                    elif event.key == pygame.K_RIGHT:
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         player.move_right = True
                     elif event.key == pygame.K_SPACE:
                         player.isJump = True
 
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                         player.move_left = False
-                    elif event.key == pygame.K_RIGHT:
+                    elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                         player.move_right = False
                     elif event.key == pygame.K_SPACE:
                         pass
+        
+        collison = player.isCollision()
+        if collison:
+            sys.exit()
+        if player.x > player.enemy_x:
+            player.score += 1
+        if player.enemy_x < -40:
+            player.enemy_x = 1450       
                     
 
 
@@ -173,6 +207,7 @@ while run:
         
 
         player.draw(win)
+        player.enemy(win)
     else:
 
         win.fill((0,0,0))
